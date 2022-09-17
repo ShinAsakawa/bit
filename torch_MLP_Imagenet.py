4ab27d316e4fc06eb1f600f355d7bb288db5d61b
Shin Asakawa <10644967+ShinAsakawa@users.noreply.github.com>
2022-09-18 01:56:31 +0900

    Update torch_MLP_Imagenet.py

--- torch_MLP_Imagenet.py
+++ torch_MLP_Imagenet.py
@@ -5,14 +5,14 @@ class MLP_Imagenet(torch.nn.Module):
     def __init__(self,
                  out_size:int=0,
                  n_hid:int=128,
-                ):
+                 device='cuda' if torch.cuda_available() else 'cpu'):
         super().__init__()
         self.n_hid = n_hid
         
-        self.fc1 = torch.nn.Linear(3 * 224 * 224, n_hid)
-        self.fc2 = torch.nn.Linear(n_hid, out_size)
-        self.relu = torch.nn.ReLU()
-        self.softmax = torch.nn.Softmax(dim=1)
+        self.fc1 = torch.nn.Linear(3 * 224 * 224, n_hid).to(device)
+        self.fc2 = torch.nn.Linear(n_hid, out_size).to(device)
+        self.relu = torch.nn.ReLU().to(device)
+        self.softmax = torch.nn.Softmax(dim=1).to(device)
 
     def forward(self, x):
         x = x.view(-1, 3 * 224 * 224)
